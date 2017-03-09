ecc19c14a797ff9afc055c798494fe06d5e83385
Ida Szubert <kszubert@haight.inf.ed.ac.uk>
2017-03-09 11:26:08 +0000

    fixed enc_states bug

--- enc_dec.py
+++ enc_dec.py
@@ -180,11 +180,14 @@ class EncoderDecoder(Chain):
             # enc_states stores the hidden state vectors of the encoder
             # this can be used for implementing attention
             if first_entry == False:
-                h_state = F.concat((self[self.lstm_enc[-1]].h, self[self.lstm_rev_enc[-1]].h), axis=1)
-                enc_states = F.concat((enc_states, h_state), axis=0)
+                forward_states = F.concat((forward_states, self[self.lstm_enc[-1]].h), axis=0)
+                backward_states = F.concat((self[self.lstm_rev_enc[-1]].h, backward_states), axis=0)
             else:
-                enc_states = F.concat((self[self.lstm_enc[-1]].h, self[self.lstm_rev_enc[-1]].h), axis=1)
+                forward_states = self[self.lstm_enc[-1]].h
+                backward_states = self[self.lstm_rev_enc[-1]].h
                 first_entry = False
+        
+        enc_states = F.concat((forward_states, backward_states), axis=1)
 
         return enc_states
 
